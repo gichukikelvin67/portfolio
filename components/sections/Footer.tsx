ac8532761910ea52a6594c29d50cae7ee7be8ac4
Gichuki Kelvin <gichukikelvin679@gmail.com>
2026-09-01 14:12:55 +0300

    changes done

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -75,7 +75,7 @@ export default function Footer() {
               </a>
 
               <a
-                href="https://www.linkedin.com/"
+                href="www.linkedin.com/in/kelvin-gichuki-285014433"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="LinkedIn"
